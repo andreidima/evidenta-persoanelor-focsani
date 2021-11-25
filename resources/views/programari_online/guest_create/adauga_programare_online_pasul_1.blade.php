04da1bdcf6f047314af8bf0e62760bf96b014c0f
‘Andrei <andrei.dima@usm.ro>
2021-11-25 16:50:59 +0200

    a

--- resources/views/programari_online/guest_create/adauga_programare_online_pasul_1.blade.php
+++ resources/views/programari_online/guest_create/adauga_programare_online_pasul_1.blade.php
@@ -41,6 +41,38 @@
                     Depunerea cererii în vederea eliberării actului de identitate
                 </h3> --}}
 
+                <p class="text-start">
+                    Pentru depunerea cererii în vederea eliberării actului de identitate/ înscrierea menţiunii de stabilire a reşedinţei pe actul de identitate.
+                    <br>
+                    <br>
+                    <b>Art.1</b> Programarea la ghișeul online în vederea obținerii actelor de identitate se face pe pagina de internet a Direcţiei de Evidenţă a Persoanelor Focșani - https://evidentapersoanelorfocsani.ro/ de către persoana interesată. Nu se pot face programări prin email, fax sau telefonic.
+                    <br>
+                    <b>Art.2. Programarea este gratuită şi netransmisibilă</b>, iar înainte de completarea programării, solicitantul se asigură că deţine toate actele necesare depunerii cererii pentru obţinerea actului de identitate.
+                    <br>
+                    <b>Art.3.</b> Durata unui interval de depunere a cererii este de 15 minute pentru fiecare persoană, <b>un interval fiind alocat unei singure persoane şi nu unei familii</b>.
+                    <br>
+                    <b>Art.4.</b> Neconcordanţa dintre datele înscrise în formularul de programare (nume, prenume, CNP) şi datele de identificare ale persoanei care se prezintă la depunerea cererii <b>duce la anularea programării</b>. Neprezentarea la data şi ora programată, neconformitatea datelor din formularul de programare sau lipsa actelor necesare duc la <b>anularea programării</b>.
+                    <br>
+                    <b>Art.5.</b> Nu se fac programări pentru zilele nelucrătoare, stabilite sau anunţate ulterior prin acte normative, zile în care nu se desfăşoară activitate de lucru cu publicul.
+                    <br>
+                    <b>Art.6.</b> Prin continuarea procedurii de programare online, solicitantul este de accord cu prelucrarea datelor cu caracter personal, în conformitate cu prevederile <b>Regulamentului (UE) 2016/679</b> pentru protecţia persoanelor fizice în ceea ce priveşte prelucrarea datelor cu caracter personal privind libera circulație a acestor date şi de abrogare a Directivei 95/46/CE.
+                    <br>
+                    <b>Art.7.</b> După realizarea programării va fi verificată căsuța de e-mail. La adresa de e-mail indicată în formularul de înregistrare se va trimite de sistem o confirmare cu data și ora programării.
+                    <br>
+                    <b>Art.8. Programul de lucru cu publicul</b> aferent activităţii de primire a cererilor pentru eliberarea actului de identitate se desfăşoară în zilele lucrătoare de luni-vineri, conform intervalului orar: Luni-08:30-16:00. Marți- 08:30-16:00; Miercuri -08:30-18:00; Joi-08:30-16:00; Vineri-08:30-16:00.
+                    <br>
+                    <b>Art.9.</b> Cererile pentru eliberarea actului de identitate se depun personal sau pe bază de procură specială obţinută de la misiunile diplomatice ori oficiile consulare ale României din străinătate. Înainte de depunerea cererii se vor achita taxele necesare pentru eliberarea actelor de identitate la casieria unităţii.
+                    <br>
+                    Este <b>obligatoriu</b> ca persoana care depune cererea să prezinte toate documentele necesare eliberării actului de identitate, conform listei de documente afişate. Actele se prezintă în original şi copie.
+                    <br>
+                    <b>Art.10.</b> Termenul de soluţionare a cererilor stabilit de Direcţia de Evidenţă a Persoanelor Craiova este fixat la 4 zile lucrătoare, excepţie făcând situaţiile în care sunt necesare verificări prevăzute de lege, în acest caz termenul fiind de 30 de zile.Funcţionarul care primeşte cererea înmânează solicitantului o dovadă care cuprinde numărul de înregistrare şi data înregistrării, precum şi data prezentării pentru eliberarea actului de identitate.
+                    <br>
+                    <b>Nu se fac programării online pentru eliberarea documentelor.</b>
+                    <br>
+                    <b><u>Art.11. Pentru cererile care vizează cetăţenii care nu au avut niciodată domiciliul în România(dobândire /redobândire cetăţenie), respectiv care solicită schimbarea domiciliului din străinătate în România nu se fac programări online, persoanele interesate urmând să se prezinte la sediul nostru.</u></b>
+                </p>
+
+
                 <h5 class="ps-3 text-start alert alert-warning">
                     Selectați o zi disponibilă din următoarele 2 luni calendaristice:
                 </h5>
