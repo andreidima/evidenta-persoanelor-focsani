94481199dce04c03b8272980b76904f5a6495bfd
unknown <andrei.dima@usm.ro>
2021-12-01 15:50:00 +0200

    a

--- resources/views/mail/programare-email.blade.php
+++ resources/views/mail/programare-email.blade.php
@@ -10,21 +10,6 @@
 
 Datele programării dumneavoastră sunt următoarele:
 <ul>
-        <li>
-            Nume: {{ $programare->nume }}
-        </li>
-        <li>
-            Email: {{ $programare->email }}
-        </li>
-        <li>
-            CNP: {{ $programare->cnp }}
-        </li>
-        <li>
-            Data: {{ \Carbon\Carbon::parse($programare->data)->dayName }}, {{ \Carbon\Carbon::parse($programare->data)->isoFormat('DD MMMM YYYY') }}
-        </li>
-        <li>
-            Ora: {{ \Carbon\Carbon::parse($programare->ora)->isoFormat('HH:mm') }}
-        </li>
 </ul>
 
 Mulțumim,<br>
@@ -34,8 +19,7 @@ Mulțumim,<br>
 {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            <br>
-            © {{ date('Y') }} {{ config('app.name') }}
+
         @endcomponent
     @endslot
 @endcomponent
